e3c18a971efc585913a9388b6d698723e82f0320
5000user5000 <37620231+5000user5000@users.noreply.github.com>
2022-10-08 18:02:49 +0800

    和學長論

--- ADL21-HW1-main/utils.py
+++ ADL21-HW1-main/utils.py
@@ -1,16 +1,15 @@
 from typing import Iterable, List
-import torch
 
 
 class Vocab:
-    PAD = "[PAD]"
-    UNK = "[UNK]"
+    PAD = "[PAD]" #把句子都padding到同個長度
+    UNK = "[UNK]" #未知單字,因為embedding.pt只記最高頻的單字
 
     def __init__(self, vocab: Iterable[str]) -> None:
         self.token2idx = {
             Vocab.PAD: 0,
             Vocab.UNK: 1,
-            **{token: i for i, token in enumerate(vocab, 2)},
+            **{token: i for i, token in enumerate(vocab, 2)}, #已知
         }
 
     @property
@@ -43,57 +42,3 @@ class Vocab:
 def pad_to_len(seqs: List[List[int]], to_len: int, padding: int) -> List[List[int]]:
     paddeds = [seq[:to_len] + [padding] * max(0, to_len - len(seq)) for seq in seqs]
     return paddeds
-
-#因為vocab的encoder似乎沒法用,只好自製
-def token_to_id(self, token: str) -> int:
-        return self.token2idx.get(token, self.unk_id)
-def encode(self, tokens: List[str]) -> List[int]:
-        return [self.token_to_id(token) for token in tokens]
-
-class DatasetIterater(object):
-    def __init__(self, batches, batch_size, device):
-        self.batch_size = batch_size
-        self.batches = batches
-        self.n_batches = len(batches) // batch_size
-        self.residue = False  # 记录batch数量是否为整数
-        if len(batches) % self.n_batches != 0:
-            self.residue = True
-        self.index = 0
-        self.device = device
-        
-
-    def _to_tensor(self, datas):
-        x = torch.LongTensor([ _['text'].split()  for _ in datas]).to(self.device)
-        y = torch.LongTensor([_['intent'] for _ in datas]).to(self.device)
-
-        # pad前的长度(超过pad_size的设为pad_size)
-        seq_len = torch.LongTensor([ len( _['text'].split() ) for _ in datas]).to(self.device)
-        return (x, seq_len), y
-
-    def __next__(self):
-        if self.residue and self.index == self.n_batches:
-            batches = self.batches[self.index * self.batch_size: len(self.batches)]
-            self.index += 1
-            batches = self._to_tensor(batches)
-            return batches
-
-        elif self.index >= self.n_batches:
-            self.index = 0
-            raise StopIteration
-        else:
-            batches = self.batches[self.index * self.batch_size: (self.index + 1) * self.batch_size]
-            self.index += 1
-            batches = self._to_tensor(batches)
-            return batches
-
-    def __iter__(self):
-        return self
-
-    def __len__(self):
-        if self.residue:
-            return self.n_batches + 1
-        else:
-            return self.n_batches
-def build_iterator(dataset, batch_size,device):
-    iter = DatasetIterater(dataset, batch_size, device)
-    return iter

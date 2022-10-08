d9f14d96da601d9215b97a38f4e2156cbfcad6c7
5000user5000 <37620231+5000user5000@users.noreply.github.com>
2022-10-08 18:02:05 +0800

    和學長論

--- ADL21-HW1-main/model.py
+++ ADL21-HW1-main/model.py
@@ -19,14 +19,12 @@ class SeqClassifier(torch.nn.Module):
     ) -> None:
         super(SeqClassifier, self).__init__()
         
-        self.embedding_pretrained = embeddings
-        self.embed = self.embedding_pretrained.size(1)\
-            if self.embedding_pretrained is not None else 300           # 字向量维度
+        self.embed = Embedding.from_pretrained(embeddings, freeze=False)     #      # 字向量维度
         #隨便設定
-        self.hidden_size = 128
-        self.num_layers = 2
-        self.dropout = 0.5
-        self.num_classes = 150
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.dropout = dropout
+        self.num_classes = num_class
         #print(self.embed)
         
         # TODO: model architecture
@@ -37,13 +35,16 @@ class SeqClassifier(torch.nn.Module):
     @property
     def encoder_output_size(self) -> int:
         # TODO: calculate the output dimension of rnn
+
         raise NotImplementedError
 
     def forward(self, batch) -> Dict[str, torch.Tensor]:
         # TODO: implement model forward
-        x, _ = x
-        out = self.embedding(x)  # [batch_size, seq_len, embeding]=[128, 32, 300]
+        #x, _ = batch
+        out = self.embed(batch)  # [batch_size, seq_len, embeding]=[128, 32, 300]
         out, _ = self.lstm(out)
         out = self.fc(out[:, -1, :])  # 句子最後时刻的 hidden state
+        
+
         return out
-        raise NotImplementedError
+        

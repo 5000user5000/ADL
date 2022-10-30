c09d8b57245be035ed1bb12d5cebe2f94ffe14d9
5000user5000 <ck10600760@gmail.com>
2022-10-30 11:48:03 +0800

    暫存

--- ADL-hw2/cs_train.py
+++ ADL-hw2/cs_train.py
@@ -18,7 +18,7 @@ import datasets
 from arguments import ModelArguments, DataTrainingArguments
 from dataset import MultiChoiceDataset
 import torch
-
+from transformers.trainer_utils import get_last_checkpoint
 
 
 
@@ -37,6 +37,7 @@ def main():
         model_args, data_args, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
     else:
         model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+
 
     # Setup logging
     logging.basicConfig(
@@ -57,6 +58,7 @@ def main():
         + f"distributed training: {bool(training_args.local_rank != -1)}, 16-bits training: {training_args.fp16}"
     )
     logger.info(f"Training/evaluation parameters {training_args}")
+
 
     
     
@@ -88,7 +90,7 @@ def main():
         cache_dir=model_args.cache_dir,
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
-    )
+    ) 
 
     # datasets
     train_dataset = MultiChoiceDataset(
@@ -126,9 +128,35 @@ def main():
         os.mkdir(model_args.ckpt_path)
         print("已生成ckpt目錄")
 
+    # Training
+    if training_args.do_train:
+        train_result = trainer.train()#resume_from_checkpoint=checkpoint
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+        metrics = train_result.metrics
+
+        max_train_samples = (
+            data_args.max_train_samples if data_args.max_train_samples is not None else len(train_dataset)
+        )
+        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+    # Evaluation
+    if training_args.do_eval:
+        logger.info("*** Evaluate ***")
+
+        metrics = trainer.evaluate()
+        max_eval_samples = data_args.max_eval_samples if data_args.max_eval_samples is not None else len(eval_dataset)
+        metrics["eval_samples"] = min(max_eval_samples, len(eval_dataset))
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
 
     # training
-    trainer.train( ) #resume_from_checkpoint=model_args.ckpt_path 因為沒有,所以先拔掉
+    # trainer.train( ) #resume_from_checkpoint=model_args.ckpt_path 因為沒有,所以先拔掉
     
     #儲存模型
     print("before save")
